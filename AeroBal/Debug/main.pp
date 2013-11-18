85e1237757704e7f4e6962872a2b425541c39fb2
luzonj <jesus.luzon@upr.edu>
2013-11-18 02:04:10 -0400

    Added ftoa function.

--- AeroBal/Debug/main.pp
+++ AeroBal/Debug/main.pp
@@ -1,11 +1,53 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdio.h
-main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/linkage.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdint.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdbool.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/yvals.h
 main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/linkage.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/_lock.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/string.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdlib.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdio.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/math.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/ymath.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/utils/uartstdio.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/inc/hw_ints.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/inc/hw_memmap.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/debug.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/fpu.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/gpio.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/interrupt.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/pin_map.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/rom.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/sysctl.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/uart.h
 
 ../main.c: 
-C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdio.h: 
-C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdint.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdbool.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/yvals.h: 
 C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/_lock.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/string.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdlib.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/math.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/ymath.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/utils/uartstdio.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/inc/hw_ints.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/inc/hw_memmap.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/debug.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/fpu.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/gpio.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/interrupt.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/pin_map.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/rom.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/sysctl.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.1.1/include/driverlib/uart.h: 
--- AeroBal/main.c
+++ AeroBal/main.c
@@ -1,28 +1,3 @@
-//*****************************************************************************
-// Testing Git.
-// uart_echo.c - Example for reading data from and writing data to the UART in
-//               an interrupt driven fashion.
-//
-// Copyright (c) 2012-2013 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-//
-// Texas Instruments (TI) is supplying this software for use solely and
-// exclusively on TI's microcontroller products. The software is owned by
-// TI and/or its suppliers, and is protected under applicable copyright
-// laws. You may not combine this software with "viral" open-source
-// software in order to form a larger program.
-//
-// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, FOR ANY REASON WHATSOEVER.
-//
-// This is part of revision 1.1 of the EK-TM4C123GXL Firmware Package.
-//
-//*****************************************************************************
-
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
@@ -43,25 +18,9 @@
 
 
 char state = 0;
-unsigned char buffer[20];
+char buffer[20];
 char count;
-//*****************************************************************************
-//
-//! \addtogroup example_list
-//! <h1>UART Echo (uart_echo)</h1>
-//!
-//! This example application utilizes the UART to echo text.  The first UART
-//! (connected to the USB debug virtual serial port on the evaluation board)
-//! will be configured in 115,200 baud, 8-n-1 mode.  All characters received on
-//! the UART are transmitted back to the UART.
-//
-//*****************************************************************************
 
-
-//*****************************************************************************
-//
-// The error routine that is called if the driver library encounters an error.
-//
 //*****************************************************************************
 #ifdef DEBUG
 void
@@ -71,11 +30,6 @@ __error__(char *pcFilename, uint32_t ui32Line)
 #endif
 
 
-//*****************************************************************************
-//
-// Send a string to the UART.
-//
-//*****************************************************************************
 void
 UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
 {
@@ -87,7 +41,7 @@ UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
 		//
 		// Write the next character to the UART.
 		//
-		ROM_UARTCharPut(UART0_BASE, *pui8Buffer++);
+		UARTCharPut(UART0_BASE, *pui8Buffer++);
 	}
 }
 
@@ -102,7 +56,7 @@ UARTBTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
 		//
 		// Write the next character to the UART.
 		//
-		ROM_UARTCharPut(UART1_BASE, *pui8Buffer++);
+		UARTCharPut(UART1_BASE, *pui8Buffer++);
 	}
 }
 
@@ -121,17 +75,13 @@ int strequals(char* one, char* two) {
 }
 void evaluateBuffer() {
 	if(strequals("bt:power=on\r\n",buffer)) {
-		turnPowerOn();
+		//turnPowerOn();
 	}
 	else if(strequals("bt:power=off\r\n",buffer)) {
-		turnPowerOn();
+		//turnPowerOn();
 	}
 }
-//*****************************************************************************
-//
-// The UART interrupt handler.
-//
-//*****************************************************************************
+
 void
 UARTIntHandler(void)
 {
@@ -140,24 +90,24 @@ UARTIntHandler(void)
 	//
 	// Get the interrupt status.
 	//
-	ui32Status = ROM_UARTIntStatus(UART0_BASE, true);
+	ui32Status = UARTIntStatus(UART0_BASE, true);
 
 	//
 	// Clear the asserted interrupts.
 	//
-	ROM_UARTIntClear(UART0_BASE, ui32Status);
+	UARTIntClear(UART0_BASE, ui32Status);
 
 	//
 	// Loop while there are characters in the receive FIFO.
 	//
 
-	while(ROM_UARTCharsAvail(UART0_BASE))
+	while(UARTCharsAvail(UART0_BASE))
 	{
 		//
 		// Read the next character from the UART and write it back to the UART.
 		//
-		ROM_UARTCharPutNonBlocking(UART1_BASE,
-				ROM_UARTCharGetNonBlocking(UART0_BASE));
+		UARTCharPutNonBlocking(UART1_BASE,
+				UARTCharGetNonBlocking(UART0_BASE));
 
 		//
 		// Blink the LED to show a character transfer is occuring.
@@ -185,23 +135,23 @@ UARTBTIntHandler(void)
 	//
 	// Get the interrupt status.
 	//
-	ui32Status = ROM_UARTIntStatus(UART1_BASE, true);
+	ui32Status = UARTIntStatus(UART1_BASE, true);
 
 	//
 	// Clear the asserted interrupts.
 	//
-	ROM_UARTIntClear(UART1_BASE, ui32Status);
+	UARTIntClear(UART1_BASE, ui32Status);
 
 	//
 	// Loop while there are characters in the receive FIFO.
 	//
 	char reset = 0;
-	while(ROM_UARTCharsAvail(UART1_BASE))
+	while(UARTCharsAvail(UART1_BASE))
 	{
 		//
 		// Read the next character from the UART and write it back to the UART.
 		//
-		char ch = ROM_UARTCharGetNonBlocking(UART1_BASE);
+		char ch = UARTCharGetNonBlocking(UART1_BASE);
 
 
 		switch(state) {
@@ -246,7 +196,7 @@ UARTBTIntHandler(void)
 		buffer[count] = ch;
 		count++;
 		reset = 0;
-		ROM_UARTCharPutNonBlocking(UART0_BASE,
+		UARTCharPutNonBlocking(UART0_BASE,
 				ch);
 
 
@@ -285,16 +235,6 @@ void itoa(int value, char* out)
 	}
 }
 
-int strToInt(char* str) {
-	int value = 0;
-	int i;
-	for(i = 0; str[i] != '\0'; i++) {
-		value *= 10;
-		value += str[i] - '0';
-	}
-	return value;
-}
-
 void concat(char* one, char* two, char* out) {
 	int i;
 	for(i = 0; one[i] != '\0'; i++) {
@@ -306,6 +246,26 @@ void concat(char* one, char* two, char* out) {
 	}
 	out[i + j] = '\0';
 }
+void ftoa(float value, char* out ) {
+	int integer = (int) value;
+	itoa(integer,out);
+	char* pt = ".";
+	concat(out,pt,out);
+	char curr[6];
+	itoa((int)((value - (int)value)*10000),curr);
+	concat(out,curr,out);
+
+}
+int strToInt(char* str) {
+	int value = 0;
+	int i;
+	for(i = 0; str[i] != '\0'; i++) {
+		value *= 10;
+		value += str[i] - '0';
+	}
+	return value;
+}
+
 int lengthOfString(char* str) {
 	int i = 0;
 	for(i = 0; str[i] != '\0'; i++);
@@ -350,38 +310,38 @@ main(void)
 	// instructions to be used within interrupt handlers, but at the expense of
 	// extra stack usage.
 	//
-	ROM_FPUEnable();
-	ROM_FPULazyStackingEnable();
+	FPUEnable();
+	FPULazyStackingEnable();
 
 	//
 	// Set the clocking to run directly from the crystal.
 	//
-	ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+	SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
 			SYSCTL_XTAL_16MHZ);
 
 	//
 	// Enable the GPIO port that is used for the on-board LED.
 	//
-	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
 	//
 	// Enable the GPIO pins for the LED (PF2).
 	//
-	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
-	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
 
 	//
 	// Enable the peripherals used by this example.
 	//
-	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
-	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 
 	//
 	// Enable processor interrupts.
 	//
-	ROM_IntMasterEnable();
+	IntMasterEnable();
 
 	//
 	// Set GPIO A0 and A1 as UART pins.
@@ -390,28 +350,28 @@ main(void)
 	GPIOPinConfigure(GPIO_PA1_U0TX);
 	GPIOPinConfigure(GPIO_PB0_U1RX);
 	GPIOPinConfigure(GPIO_PB1_U1TX);
-	ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-	ROM_GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+	GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
 	//
 	// Configure the UART for 115,200, 8-N-1 operation.
 	//
-	ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200,
+	UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
 			(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
 					UART_CONFIG_PAR_NONE));
 
-	ROM_UARTConfigSetExpClk(UART1_BASE, ROM_SysCtlClockGet(), 9600,
+	UARTConfigSetExpClk(UART1_BASE, SysCtlClockGet(), 9600,
 			(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
 					UART_CONFIG_PAR_NONE));
 
 	//
 	// Enable the UART interrupt.
 	//
-	ROM_IntEnable(INT_UART0);
-	ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+	IntEnable(INT_UART0);
+	UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
 
-	ROM_IntEnable(INT_UART1);
-	ROM_UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
+	IntEnable(INT_UART1);
+	UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
 
 	//
 	// Prompt for text to be entered.
@@ -423,10 +383,10 @@ main(void)
 	//
 	// Loop forever echoing data through the UART.
 	//
-
+	char floa[20];
+	ftoa(34.5,floa);
+	puts(floa);
 	while(1)
 	{
-		send("drag","445");
-		sendDrag(234);
 	}
 }
