8fdd6d2eefc1cefcd95211f97a9ee4eb4dd308d2
allanos1 <anthony.llanos@upr.edu>
2013-11-28 06:42:52 -0400

    Updating to latest project version,

--- AeroBal/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"/sensorlib/ccs/Debug/sensorlib.lib" -l"C:/ti/TivaWare_C_Series-1.1/driverlib/ccs/Debug/driverlib.lib"
-
--- AeroBalv3/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/ti/TivaWare_C_Series-1.0/examples/boards/ek-tm4c123gxl-boostxl-senshub/pressure_bmp180/ccs/../../../../../driverlib/ccs/Debug/driverlib.lib" -l"C:/ti/TivaWare_C_Series-1.0/examples/boards/ek-tm4c123gxl-boostxl-senshub/pressure_bmp180/ccs/../../../../../sensorlib/ccs/Debug/sensorlib.lib" -l"libc.a"
-
